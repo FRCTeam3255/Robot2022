11581ba3ce40d17acab4951d1e6b4f726ae9a89b
npNiles <95945925+npNiles@users.noreply.github.com>
2022-01-26 18:04:54 -0800

    Updated addRequirements()

--- src/main/java/frc/robot/commands/Shooter/ShootCargoIf.java
+++ src/main/java/frc/robot/commands/Shooter/ShootCargoIf.java
@@ -5,24 +5,42 @@
 package frc.robot.commands.Shooter;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotPreferences;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Transfer;
 
 public class ShootCargoIf extends CommandBase {
+  Shooter shooter;
+  Transfer transfer;
+
   /** Creates a new ShootCargoIf. */
-  public ShootCargoIf() {
+  public ShootCargoIf(Shooter sub_shooter, Transfer sub_transfer) {
     // Use addRequirements() here to declare subsystem dependencies.
+
+    shooter = sub_shooter;
+    transfer = sub_transfer;
+
+    addRequirements(shooter, transfer);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (shooter.getShooterVelocity() >= RobotPreferences.ShooterPrefs.shooterMotorTargetVelocity.getValue()) {
+      // START TRANSFER, waiting for transfer subsystem to be uploded
+    }
+    ;
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
