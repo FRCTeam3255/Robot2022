f3461d35d74eee7119de257f1cb2807671e952df
Software 1 <frcteam3255@gmail.com>
2022-02-05 15:04:34 -0800

    Transfer bug fixes
    closes #91

--- src/main/java/frc/robot/subsystems/Transfer.java
+++ src/main/java/frc/robot/subsystems/Transfer.java
@@ -9,16 +9,10 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-//if  rev robotics import is red, use ctrl shift p and clean java workspace
-import com.revrobotics.ColorSensorV3;
-
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
-import frc.robot.RobotMap.IntakeMap;
 
 public class Transfer extends SubsystemBase {
   /**
@@ -28,15 +22,13 @@ public class Transfer extends SubsystemBase {
   private TalonFX topBeltMotor;
   private TalonFX bottomBeltMotor;
   private TalonFX entranceBeltMotor;
-  private ColorSensorV3 colorSensor;
-  private final I2C.Port i2cPort = I2C.Port.kOnboard;
   private DigitalInput transferTopLimitSwitch;
   private DigitalInput transferBottomLimitSwitch;
 
   public Transfer() {
     topBeltMotor = new TalonFX(RobotMap.TransferMap.TOP_BELT_MOTOR_CAN);
     bottomBeltMotor = new TalonFX(RobotMap.TransferMap.BOTTOM_BELT_MOTOR_CAN);
-    colorSensor = new ColorSensorV3(i2cPort);
+    entranceBeltMotor = new TalonFX(RobotMap.TransferMap.MIDDLE_BELT_MOTOR_CAN);
     transferTopLimitSwitch = new DigitalInput(RobotMap.TransferMap.TRANSFER_TOP_LIMIT_SWITCH_DIO);
     transferBottomLimitSwitch = new DigitalInput(RobotMap.TransferMap.TRANSFER_BOTTOM_LIMIT_SWITCH_DIO);
   }
