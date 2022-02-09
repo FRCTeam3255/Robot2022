2b189d4228be7fbb44fd1f58a59819bf24ea2ad9
Alice <90939494+ACat701@users.noreply.github.com>
2022-02-08 16:23:14 -0800

    Changed Turret Safety to a Soft Limit

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -9,6 +9,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.RobotPreferences;
 import frc.robot.RobotMap;
@@ -28,11 +29,21 @@ public class Turret extends SubsystemBase {
   // SET TO FACTORY DEFAULT
   public void configure() {
     turretMotor.configFactoryDefault();
+
+    // soft limit
+    turretMotor.configForwardSoftLimitThreshold(RobotPreferences.TurretPrefs.turretMaxEncoderCount.getValue(), 0);
+    turretMotor.configReverseSoftLimitThreshold(RobotPreferences.TurretPrefs.turretMinEncoderCount.getValue(), 0);
+    turretMotor.configForwardSoftLimitEnable(true, 0);
+    turretMotor.configReverseSoftLimitEnable(true, 0);
   }
 
   // RESETS COUNT FOR ENCONDERS
   public void resetEncoderCounts() {
     turretMotor.setSelectedSensorPosition(0);
+
+    // soft limit
+    turretMotor.configForwardSoftLimitThreshold(RobotPreferences.TurretPrefs.turretMaxEncoderCount.getValue(), 0);
+    turretMotor.configReverseSoftLimitThreshold(RobotPreferences.TurretPrefs.turretMinEncoderCount.getValue(), 0);
   }
 
   // GETS AND RETURNS COUNT FOR ENCONDERS
@@ -43,13 +54,7 @@ public class Turret extends SubsystemBase {
   public void setTurretSpeed(double a_rotate) {
     double rotate = a_rotate;
 
-    if ((getTurretMotorEncoderCount() * RobotPreferences.TurretPrefs.turretMotorEncoderAngleMultiplier.getValue() < 359
-        && RobotContainer.coDriverStick.getRightStickX() > 0)
-        || (getTurretMotorEncoderCount() * RobotPreferences.TurretPrefs.turretMotorEncoderAngleMultiplier.getValue() > 1
-            && RobotContainer.coDriverStick.getRightStickX() < 0)) {
-      turretMotor.set(ControlMode.PercentOutput, rotate);
-
-    }
+    turretMotor.set(ControlMode.PercentOutput, rotate);
 
   }
 
