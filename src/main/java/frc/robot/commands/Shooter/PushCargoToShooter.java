23bf35e7e81a55256cc01cc9a70dbcf387b89b46
Adam-Walls077 <83879523+Adam-Walls077@users.noreply.github.com>
2022-02-12 12:07:56 -0800

    Update src/main/java/frc/robot/commands/Shooter/PushCargoToShooter.java
    Co-authored-by: Tayler Uva <8679670+TaylerUva@users.noreply.github.com>

--- src/main/java/frc/robot/commands/Shooter/PushCargoToShooter.java
+++ src/main/java/frc/robot/commands/Shooter/PushCargoToShooter.java
@@ -32,7 +32,7 @@ public class PushCargoToShooter extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // If the Shooter speed is greater than or equal to the Targeted velocity than
+    // If the Shooter speed is greater than or equal to the Targeted velocity then
     // Transfer BeltMotors speed up
     if (shooter.getShooterVelocity() >= RobotPreferences.ShooterPrefs.shooterMotorTargetVelocity.getValue()) {
       // transfer.setTransferMotorSpeed(RobotPreferences.TransferPrefs.transferMotorSpeed);
