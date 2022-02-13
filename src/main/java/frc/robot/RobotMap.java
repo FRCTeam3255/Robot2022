daf8eba2ec3022c47b391aab50ae96f7218f0b0b
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2022-02-13 15:29:24 -0800

    revert RobotMap to main

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -43,7 +43,7 @@ public final class RobotMap {
         public static final int TOP_BELT_MOTOR_CAN = 30;
         public static final int BOTTOM_BELT_MOTOR_CAN = 31;
         public static final int TRANSFER_TOP_LIMIT_SWITCH_DIO = 1;
-        public static final int TRANSFER_BOTTOM_LIMIT_SWITCH_DIO = 40;
+        public static final int TRANSFER_BOTTOM_LIMIT_SWITCH_DIO = 2;
 
     }
 
