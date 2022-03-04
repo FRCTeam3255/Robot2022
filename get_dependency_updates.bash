6bf6a800dc78f8e4607e8788f01a78fe90baaee5
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2022-03-03 18:50:04 -0800

    added dependency checking

---
+++ get_dependency_updates.bash
@@ -0,0 +1,20 @@
+./gradlew dependencyUpdates -DoutputFormatter=json 
+outdated=$(jq '[
+.outdated.dependencies|.[] |
+  select (
+    (.group != "com.github.ben-manes.versions") and
+    (.group != "org.ejml") and
+    (.group != "com.fasterxml.jackson.core") and
+    (.group != "junit")
+  ) |
+  {vendor: .group, current_version:.version, newest_version:.available.milestone}
+]' build/dependencyUpdates/report.json |
+  jq 'unique_by(.vendor)|unique_by(.newest_version) | .[]' # not the best way to dedup
+)
+if [  -n "$outdated" ]; then
+    echo 'The following dependencies are out of date!'
+    jq '.' <<< $outdated
+    exit 1
+else
+    echo 'All dependencies up to date'
+fi
