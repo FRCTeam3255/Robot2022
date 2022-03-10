347f047d1acb10ee82e98f4bfaf56a7d73910f5c
Alice <90939494+ACat701@users.noreply.github.com>
2022-03-09 17:08:54 -0800

    Created RunSpoolForward Command

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -187,6 +187,9 @@ public final class RobotPreferences {
   }
 
   public static final class ClimberPrefs {
+
+    public static final SN_DoublePreference climberSpoolForwardSpeed = new SN_DoublePreference(
+        "climberSpoolForwardSpeed", 0.5);
     public static final SN_DoublePreference climberMotorSpeed = new SN_DoublePreference("climberMotorSpeed", 0.5);
     public static final SN_DoublePreference climberMaxEncoderCountPerpendicular = new SN_DoublePreference(
         "climberMaxEncoderCount", 150000);
