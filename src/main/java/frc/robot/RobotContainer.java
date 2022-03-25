87b3d25d1caa9bcc679c5a0973a647cb6b4ed0bf
Alice <90939494+ACat701@users.noreply.github.com>
2022-03-24 18:31:36 -0700

    Removed Unused Import (Which also made builds fail)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.Drivetrain.*;
-import frc.robot.commands.Hood.*;
 import frc.robot.commands.Turret.*;
 import frc.robot.commands.Vision.SetGoalRPM;
 import frc.robot.commands.Intake.*;
